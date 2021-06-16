641d9512bd7aefd616a25facbafe3643e5507ef4
Jose Jimenez <josejimenez826@gmail.com>
2021-06-15 20:42:08 -0700

    testing code from examples

---
+++ public/online.js
@@ -0,0 +1,79 @@
+const { response } = require("express");
+
+let db;
+let budgetVersion;
+
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+    console.log("Upgrade needed in IndexDB");
+    
+    const {oldVersion} = e;
+    const newVersion = e.newVersion || db.version;
+
+    console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+    db = e.target.result;
+
+    if(db.objectStoreNames.length === 0){
+        db.createObjectStore('BudgetStore', {autoIncrement: true});
+    }
+}
+
+request.onerror = function (e) {
+    console.log(`Woops! ${e.traget.errrorCode}`)
+}
+
+function checkDatabase() {
+    console.log('check db invoked')
+
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+    const store = transaction.objectStore('BudgetStore');
+
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if(getAll.result.length > 0){
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, test/plain, */*', 
+                    'Content-Type':'application/json',
+                }
+            }).then((response) => response.json()).then((res) => {
+                if(res.length !== 0){
+                    transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+                    const currentStore = transaction.objectStore('BudgetStore');
+
+                    currentStore.clear();
+                    console.log('Clearing store');
+                }
+            })
+        }
+    }
+}
+
+request.onsuccess = function (e) {
+    console.log('sucess');
+    db = e.target.result;
+
+    if(navigator.online){
+        conolse.log('Backend online!');
+        checkDatabase();
+    }
+}
+
+const saveRecord = (record) => {
+    console.log('Save record invoed');
+
+    const transaction = db.transaction(['BudgetStore'], 'readwrite')
+
+    const store = transaction.objectStore('BudgetStore');
+
+    store.add(record);
+}
+
+window.addEventListener('online', checkDatabase);
