23c96434f6ef379268648d67c47909e7a8fc5097
Jose Jimenez <josejimenez826@gmail.com>
2021-06-16 18:34:01 -0700

    worked on adding and deleting data in indexdb

--- public/online.js
+++ public/online.js
@@ -3,7 +3,7 @@ const request = window.indexedDB.open('BudgetDB', 1);
 request.onupgradeneeded = ({target}) => {
     console.log("upgrade")
     const db = target.result;
-    const objectStore = db.createObjectStore("BudgetDB")
+    const objectStore = db.createObjectStore("BudgetDB", {keyPath: "listID"})
 
     objectStore.createIndex("budget", "budget")
 }
@@ -19,10 +19,39 @@ const offlineData = () => {
     subBtn.addEventListener("click", offlineSub)
 }
 
+// let transaction = {
+//     name: nameEl.value,
+//     value: amountEl.value,
+//     date: new Date().toISOString()
+//   };
 const offlineAdd = () =>{
     // get data to add to indexdb when offline
-    console.log("add")
-    // find out what kind of data is being sent to the db
+    let nameEl = document.querySelector("#t-name");
+    let amountEl = document.querySelector("#t-amount");
+    // let errorEl = document.querySelector(".form .error");
+    // console.log("add")
+    let data = {
+        name: nameEl.value,
+        value: amountEl.value,
+        date: new Date().toISOString()
+    };
+
+    console.log(data)
+
+    const db  = request.result;
+    const transaction = db.transaction(["BudgetDB"], "readwrite")
+    const budgetStore = transaction.objectStore("BudgetDB")
+    const budgetIndex = budgetStore.index("budget")
+    const budgetAuto = budgetStore.autoIncrement
+
+    budgetStore.add(data)
+
+    const getRequest = budgetStore.get('1')
+    getRequest.onsuccess = () => {
+        console.log(getRequest.result)
+    }
+    // thinking of adding a global variable to keep track of id for key index
+    // then restart teh variable when back online.
 }
 
 const offlineSub = () => {
